6df70339357ae96e369ecf747049f6a788512f3c
Julian Negrete <junegr@ymail.com>
2020-11-19 14:46:26 -0600

    Commmentated out third commit

--- EgitTesting/src/HelloEgit.java
+++ EgitTesting/src/HelloEgit.java
@@ -4,6 +4,7 @@ public class HelloEgit {
 	public static void main(String[] args) {
 		System.out.println("output");
 		System.out.println("Successful Second Commit");
+		//System.out.println("Successful third Commit");
 	}
 
 }
